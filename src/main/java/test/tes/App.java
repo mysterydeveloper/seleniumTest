e5278e6365d2bdb06edcab0034c091362ff6b759
unknown <mysterydeveloper>
2018-02-17 19:50:10 +0000

    First commit

---
+++ src/main/java/test/tes/App.java
@@ -0,0 +1,13 @@
+package test.tes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/test/tes/UntitledTestCase.java
@@ -0,0 +1,85 @@
+package test.tes;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UntitledTestCase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+		  WebDriverManager.chromedriver().setup();
+		  
+
+		  ChromeOptions chromeOptions = new ChromeOptions();
+	      //chromeOptions.addArguments("--headless");
+
+	       driver = new ChromeDriver(chromeOptions);
+    baseUrl = "https://www.google.ie";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+	  driver.get("https://www.google.ie/?gws_rd=cr&dcr=0&ei=qlqIWpvGHIPQgAbTuLC4DQ");
+	    driver.findElement(By.id("lst-ib")).clear();
+	    driver.findElement(By.id("lst-ib")).sendKeys("test");
+	    driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
